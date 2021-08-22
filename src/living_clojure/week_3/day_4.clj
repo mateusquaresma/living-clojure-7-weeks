e4fe501e4b7633c137afe50c5841a7e7224460c9
mateusquaresma <mateus.quaresma@protonmail.com>
2021-08-22 12:08:19 -0300

    Week 3, day 4

--- src/living_clojure/week_3/day_4.clj
+++ src/living_clojure/week_3/day_4.clj
@@ -12,8 +12,38 @@
              (conj primes i)
              (inc limit)))))
 
-(= (sieve 2) [2 3])
+(defn prime? [n]
+  (let [range-n (range 2 n)
+        divisors (filter some? (map #(when (= 0 (rem n %)) %) range-n))]
+    (zero? (count divisors))))
+(defn prime-gen [n]
+  (take n (filter prime? (iterate inc 2))))
 
-(= (sieve 5) [2 3 5 7 11])
 
-(= (last (sieve 100)) 541)
+(= (prime-gen 2) [2 3])
+
+(= (prime-gen 5) [2 3 5 7 11])
+
+(= (last (prime-gen 100)) 541)
+
+; Anagram finder
+
+(defn anagram? [a b]
+  (let [a-chars (set a)
+        b-chars (set b)]
+    (= a-chars b-chars)))
+
+(defn anagram [a b]
+  (when (anagram? a b) b))
+
+(defn anagram-finder [words]
+  (let [candidates (for [w words]
+                     (let [anagram-w (partial anagram w)]
+                       (map anagram-w words)))
+        non-nil-candidates (filter some? candidates)] ))
+
+(= (__ ["meat" "mat" "team" "mate" "eat"])
+   #{#{"meat" "team" "mate"}})
+
+(= (__ ["veer" "lake" "item" "kale" "mite" "ever"])
+   #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}})
